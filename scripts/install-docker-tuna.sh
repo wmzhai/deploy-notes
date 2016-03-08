d096850be69a5ac05116927013280ca9cc1b681b
Weiming Zhai <wmzhai@gmail.com>
2016-03-08 12:20:08 +0800

    install docker with daocloud

---
+++ scripts/install-docker-tuna.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 查看docker是否已经安装
+set +e
+haveDocker=$(docker version | grep "version")
+set -e  # 如果一个指令返回非零状态，则退出
+
+if [ ! "$haveDocker" ]; then
+
+  # Remove the lock
+  set +e
+  sudo rm /var/lib/dpkg/lock > /dev/null
+  sudo rm /var/cache/apt/archives/lock > /dev/null
+  sudo dpkg --configure -a
+  set -e
+
+  # Add TUNA Mirror
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo "deb https://mirrors.tuna.tsinghua.edu.cn/docker/apt/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+
+  # Required to update system
+  sudo apt-get update
+
+  # Install docker
+  sudo apt-get install docker-engine -y
+fi
