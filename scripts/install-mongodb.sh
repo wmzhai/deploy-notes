d936a7ddad92e9dfba3e364010e1fd095d890cde
Weiming Zhai <wmzhai@gmail.com>
2016-03-09 19:22:40 +0800

    mongodb

--- scripts/install-mongodb.sh
+++ scripts/install-mongodb.sh
@@ -15,6 +15,5 @@ sudo docker run \
   --restart=always \
   --publish=127.0.0.1:27017:27017 \
   --volume=/var/lib/mongodb:/data/db \
-  --volume=/opt/mongodb/mongodb.conf:/mongodb.conf \
   --name=mongodb \
-  mongo mongod -f /mongodb.conf
+  mongo mongod
